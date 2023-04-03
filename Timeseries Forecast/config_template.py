50e8c835a63d4327ffe8da304225deb4f8f9b90b
sfc-gh-knadadur <108832646+sfc-gh-knadadur@users.noreply.github.com>
2023-04-02 23:45:51 -0700

    Add files via upload

---
+++ Timeseries Forecast/config_template.py
@@ -0,0 +1,9 @@
+snowflake_conn_prop_local = {
+   "account": "snowflakeaccount",
+   "user": "user",
+   "password": "password",
+   "database": "database",
+   "schema": "schema",
+   "warehouse": "warehouse",
+    "role":"role",
+}
